09c90f75d82f212af7fb6bd2aea227db1ffb6def
hah1992 <18695612201@163.com>
2015-11-02 09:26:57 +0800

    Initial Commit

---
+++ AHTextView/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  AHTextView
+//
+//  Created by 黄安华 on 15/11/2.
+//  Copyright (c) 2015年 黄安华. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ AHTextView/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  AHTextView
+//
+//  Created by 黄安华 on 15/11/2.
+//  Copyright (c) 2015年 黄安华. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
