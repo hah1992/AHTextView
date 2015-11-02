0ae8996c93cc91abfc2040e8db89fbbf20c1eeb4
hah1992 <18695612201@163.com>
2015-11-02 15:48:47 +0800

    awesome things

--- AHTextView/AppDelegate.h
+++ AHTextView/AppDelegate.h
@@ -5,7 +5,7 @@
 //  Created by 黄安华 on 15/11/2.
 //  Copyright (c) 2015年 黄安华. All rights reserved.
 //
-
+// add some aewsome things
 #import <UIKit/UIKit.h>
 
 @interface AppDelegate : UIResponder <UIApplicationDelegate>
